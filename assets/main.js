45ee2a1698e901f5e41ab55bf4185a093183a563
Loris Matteri <loris.matteri93@gmail.com>
2020-12-04 15:38:34 +0100

    snack 1

--- assets/main.js
+++ assets/main.js
@@ -1,1 +1,78 @@
-console.log('ciaone');
+//SNACK 1
+//Crea 10 oggetti che rappresentano una zucchina, indicando per ognuna varietà, peso e lunghezza.
+//Calcola quanto pesano tutte le zucchine.
+
+var zucchine = [
+  {
+    varieta: 'zucchina chiara di Faenza',
+    peso: 1,
+    lunghezza: 15
+  },
+  
+  {
+    varieta: 'zucchina scura di Faenza',
+    peso: 2,
+    lunghezza: 15
+  },
+  
+  {
+    varieta: 'zucchina gialla di Faenza',
+    peso: 3,
+    lunghezza: 15
+  },
+  
+  {
+    varieta: 'zucchina arancione di Faenza',
+    peso: 4,
+    lunghezza: 15
+  },
+  
+  {
+    varieta: 'zucchina rossa di Faenza',
+    peso: 5,
+    lunghezza: 15
+  },
+
+  {
+    varieta: 'zucchina verde di Faenza',
+    peso: 6,
+    lunghezza: 15
+  },
+  
+  {
+    varieta: 'zucchina nera di Faenza',
+    peso: 7,
+    lunghezza: 15
+  },
+  
+  {
+    varieta: 'zucchina grossa di Faenza',
+    peso: 8,
+    lunghezza: 15
+  },
+  
+  {
+    varieta: 'zucchina piccola di Faenza',
+    peso: 9,
+    lunghezza: 15
+  },
+  
+  {
+    varieta: 'zucchina media di Faenza',
+    peso: 10,
+    lunghezza: 15
+  },
+
+]
+
+console.log(zucchine);
+  
+var somma = 0;
+
+for (var i = 0; i < zucchine.length; i++) {
+    somma = somma + zucchine[i].peso;
+}
+
+console.log(somma);
+  
+
